4a64eeece1300fd1b8aafc55d2d2aec098208b0f
Richard Paul <5623060+richardnpaul@users.noreply.github.com>
2026-02-17 18:58:08 +0000

    Fix issues with flutter analyze

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'services/background_service.dart';
+import 'services/storage_service.dart';
 import 'ui/dashboard_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  // Initialize Storage
+  final prefs = await SharedPreferences.getInstance();
+  final storageService = StorageService(prefs);
+
   // Initialize Background Service
   await initializeService();
 
-  runApp(const ProviderScope(child: IntervalTimerApp()));
+  runApp(
+    ProviderScope(
+      overrides: [storageServiceProvider.overrideWithValue(storageService)],
+      child: const IntervalTimerApp(),
+    ),
+  );
 }
 
 class IntervalTimerApp extends StatelessWidget {
